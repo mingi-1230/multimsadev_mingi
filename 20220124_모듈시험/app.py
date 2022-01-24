95a9237a7910691d2f41e79d2025ceb3564b93ca
mingi-1230 <98008704+mingi-1230@users.noreply.github.com>
2022-01-24 23:11:24 +0900

    해냈다..!
    와 드디어 했다... 이렇게 쉬운 걸 제대로 이해 못해서 버벅였다....
    일단 공공데이터 자료를 불러오는 건 request.get()을 통해서 간단하게 불러올 수 있고
    불러온 파일을 플라스크를 통해서 웹브라우저에 연결시키는 것도 했다.
    
    이제 해야할 건 json을 key값별로 분류하여 정제를 한 후에 플라스크를 통해 정제된 데이터를 재연결시키기만 하면 된다.
    플라스크에서는 한글 적용이 안되서 한글이 나오게만 설정하면 될 것 같다.

--- 20220124_모듈시험/app.py
+++ 20220124_모듈시험/app.py
@@ -1,25 +1,34 @@
+from flask import Flask
 import json
 import requests
-from flask import Flask
 
-app = Flask(__name__)
+app = Flask(__name__) # flask 프로그램 시작 기본 값은 = app.py 파일을 생성
+
 #함수 선언 
 # 시작할 때 경로(route) 선언해야함
+@app.route("/hello") # 웹 사이트 경로를 지정 - 앞에서 선언한 app 변수를 사용
+def FlaskLab(): # 요청 - 메서드(함수) 이름 요청에서 사용하는 것
+    return "Flask 데이터 응답" # 응답 -  return 에서 돌려주는 데이터가 응답
 
-@app.route("/")
-def FlaskData():#startPage, pageCount, address): # 요청 받음
-    keyValue = "iQRdwNfyr4pUnPYTin2f07Wps5tqeblg1cA%2BihzCLinT63BwzOTOTdy63rJDOBIDBMjjaElmpWQb4X8Cdh2NDQ%3D%3D"   
-    dataURL = "https://api.odcloud.kr/api/apnmOrg/v1/list?"
-    dataURL += "page=" + str(1) + "&perPage=" + str(10)
-    #dataURL += "&returnType=JSON"
-    dataURL += "&cond%5BorgZipaddr%3A%3ALIKE%5D=%EA%B4%80%EC%95%85%EA%B5%AC" # 또는 본인 사는 지역구
-    dataURL += "&serviceKey=" + keyValue
-    # tempURL = r"https://api.odcloud.kr/api/apnmOrg/v1/list?page=1&perPage=10
-    # &cond=""
-    # &serviceKey=""
-    # https://api.odcloud.kr/api/apnmOrg/v1/list?페이지 그리고 인증 값 연결
-    dataResult = requests.get(dataURL)
-    #공공데이터 요청 후 데이터 받기 : flask - request / requests 기능 사용"
-    return json.loads(dataResult) # 공공데이터 결과 값 응답
 
-print(FlaskData())
+
+@app.route("/data1")
+def data(): #(page1, page2, address):
+    end_point = "https://api.odcloud.kr/api/apnmOrg/v1/list?"
+    key = "iQRdwNfyr4pUnPYTin2f07Wps5tqeblg1cA%2BihzCLinT63BwzOTOTdy63rJDOBIDBMjjaElmpWQb4X8Cdh2NDQ%3D%3D"
+    parameters = "page=" + str(1)
+    parameters += "&perPage=" + str(10)
+    parameters += "&returnType=JSON"
+    parameters += "&cond%5BorgZipaddr%3A%3ALIKE%5D=" + "관악구"
+    parameters += "&serviceKey=" + key
+    url = end_point + parameters
+
+    content = requests.get(url).content
+
+    return json.loads(content, encoding = "utf-8")
+
+
+#a = data(1, 10, "관악구")
+
+if __name__ == '__main__':
+    app.run(debug=True)
